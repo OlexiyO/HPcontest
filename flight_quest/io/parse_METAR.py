6ba122206a1245b34b2ebb9c485fb982f287635a
Pavlo Shelyazhenko <pavlo.shelyazhenko@gmail.com>
2013-01-13 14:00:05 -0800

    Fix METAR files processing.
    Runway data included only for low visibility, if it is absent - that
    just means visibility is over 6000 ft. Also, use indices for joins.

--- flight_quest/io/parse_METAR.py
+++ flight_quest/io/parse_METAR.py
@@ -2,7 +2,9 @@ import os
 import dateutil
 import pandas as pd
 import numpy as np
+import itertools
 from flight_quest import util
+from flight_quest.io import local_constants
 
 
 """This module parses data in metar directory.
@@ -31,13 +33,12 @@ def GetHeight(desc):
 
 
 # Later entries mean better conditions
-CLOUD_TYPES = [
-  (0, 'vertical visibility'),
-  (1, 'overcast layer'),
-  (2, 'broken layer'),
-  (3, 'scattered layer'),
-  (4, 'few clouds'),
-  (5, 'clear'),]
+CLOUD_TYPES = [(0, 'vertical visibility'),
+               (1, 'overcast layer'),
+               (2, 'broken layer'),
+               (3, 'scattered layer'),
+               (4, 'few clouds'),
+               (5, 'clear')]
 
 
 def GetCloudsType(desc):
@@ -49,25 +50,24 @@ def GetCloudsType(desc):
 
 
 def ParseSkys(sky_filepath):
-  sky_df = pd.read_csv(sky_filepath)
+  sky_df = pd.read_csv(sky_filepath, index_col='id')
+
+  CLOUDS_COL, HEIGHT_COL, CUMULUS_COL = range(3)
 
   data = []
   prev = {}
-  for row in sky_df.values:
-    metar_id = row[1]
-    desc = row[2].lower()
-    new_data = [metar_id, GetCloudsType(desc), GetHeight(desc), Cumulus(desc)]
+  for metar_id, desc in itertools.izip(sky_df.metar_reports_id, sky_df.sky_condition):
+    desc = desc.lower()
+    new_data = (metar_id, [GetCloudsType(desc), GetHeight(desc), Cumulus(desc)])
     next_ind = len(data)
     ind = prev.get(metar_id, -1)
     if ind != -1:
       # Take max cumulus
-      data[ind][3] = max(data[ind][3], new_data[3])
-      if data[ind][2] < new_data[2]:
-        # Take lower layer of clouds + 1
-        data[ind][2] = new_data[2] - 1
-        data[ind][1] = new_data[1]
-      else:
-        data[ind][2] -= 1
+      data[ind][1][CUMULUS_COL] = max(data[ind][1][CUMULUS_COL], new_data[1][CUMULUS_COL])
+      # Select data for maximum height.
+      if data[ind][1][HEIGHT_COL] < new_data[1][HEIGHT_COL]:
+        data[ind][1][HEIGHT_COL] = new_data[HEIGHT_COL]
+        data[ind][1][CLOUDS_COL] = new_data[CLOUDS_COL]
     else:
       prev[metar_id] = next_ind
       data.append(new_data)
@@ -76,7 +76,7 @@ def ParseSkys(sky_filepath):
 
 def ProcessSky(sky_filepath):
   sky_2d_array = ParseSkys(sky_filepath)
-  return pd.DataFrame(sky_2d_array, columns=[ID_FIELD, 'clouds_type', 'clouds_height', 'cumulus'])
+  return pd.DataFrame.from_items(sky_2d_array, orient='index', columns=['clouds_type', 'clouds_height', 'cumulus'])
 
 
 def ParseWindDirectionDelta(s):
@@ -94,19 +94,29 @@ def ParseWindDirectionDelta(s):
 
 
 def ProcessReportsCombined(main_filepath, t0):
-  main_df = pd.read_csv(main_filepath)
+  main_df = pd.read_csv(main_filepath, index_col=ID_FIELD)
   main_df['time'] = main_df['date_time_issued'].map(lambda x: util.DateStrToMinutes(x, t0))
   main_df['is_wind_variable'] = main_df['is_wind_direction_variable'].map(lambda x: 1 if x == 'T' else 0)
   main_df['wind_direction_delta'] = main_df['variable_wind_direction'].map(ParseWindDirectionDelta)
-  useful_cols = [ID_FIELD, 'weather_station_code', 'time', 'is_wind_variable', 'wind_gusts',
-                 'wind_speed', 'wind_direction_delta', 'visibility', 'temperature', 'dewpoint',
-                 'altimeter', 'sea_level_pressure']
+  useful_cols = ['weather_station_code',
+                 'time',
+                 'is_wind_variable',
+                 'wind_gusts',
+                 'wind_speed',
+                 'wind_direction_delta',
+                 'visibility',
+                 'temperature',
+                 'dewpoint',
+                 'altimeter',
+                 'sea_level_pressure']
   return main_df[useful_cols]
 
 
 def ProcessRunway(ID_FIELD, runway_filepath):
-  runway_df = pd.read_csv(runway_filepath)
-  return runway_df.drop_duplicates(cols=[ID_FIELD])[[ID_FIELD, 'min_visible', 'max_visible']]
+  runway_df = pd.read_csv(runway_filepath, index_col='id')
+  runway_df.min_visible = util.OR(runway_df.min_visible, runway_df.max_visible)
+  grouped = runway_df.groupby(ID_FIELD)
+  return grouped.agg({'min_visible': min, 'max_visible': max})
 
 
 def MergeMETARFiles(base_dir, output_dir, t0):
@@ -115,11 +125,22 @@ def MergeMETARFiles(base_dir, output_dir, t0):
   runway_filepath = os.path.join(input_dir, 'flightstats_metarrunwaygroups_combined.csv')
   main_filepath = os.path.join(input_dir, 'flightstats_metarreports_combined.csv')
 
+  print 'Starting METAR files for', base_dir, '...'
   main_df = ProcessReportsCombined(main_filepath, t0)
+  print 'Reports processed...'
   runway_df = ProcessRunway(ID_FIELD, runway_filepath)
+  print 'Runway processed...'
   sky_df = ProcessSky(sky_filepath)
+  print 'Sky processed...'
 
-  main_df = main_df.merge(runway_df)
-  main_df = main_df.merge(sky_df, on=ID_FIELD)
-  main_df.to_csv(os.path.join(output_dir, 'metar_weather.csv'), index=False)
-  print 'Converted METAR files for %s' % base_dir
+  main_df = main_df.join(sky_df).join(runway_df)
+  main_df.to_csv(os.path.join(output_dir, 'metar_weather.csv'))
+  print 'Converted METAR files for', base_dir
+
+if __name__ == '__main__':
+  parent_dir = local_constants.PARENT_DATA_DIR
+  date_str = '2012-11-12'
+  t0 = dateutil.parser.parse(date_str).replace(tzinfo=dateutil.tz.tzutc())
+  base_dir = os.path.join(parent_dir, date_str.replace('-', '_'))
+  out_dir = os.path.join(base_dir, 'metartest')
+  MergeMETARFiles(base_dir, out_dir, t0)
