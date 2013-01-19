090202098321428da0cd8322cc36b24a39e2f68e
Pavlo Shelyazhenko <pavlo.shelyazhenko@gmail.com>
2013-01-19 13:43:29 -0800

    Join weather information to the model.
    Joines some weather information about destination airport to each
    flight. Selects only the information known at cutoff time.

--- flight_quest/model/generate_input.py
+++ flight_quest/model/generate_input.py
@@ -23,6 +23,34 @@ def GenerateWeatherPerFlight(df_joined, df_weather, output_file):
       GetAirportCode, na_action='ignore')
 
 
+INTERESTING_WEATHER_COLUMNS = ['cumulus', 'visibility', 'temperature']
+
+def ModifyWeatherColumnName(col):
+  return 'last_' + col
+
+def AddLastWeatherReportInDestination(df_joined, df_weather):
+  grouped_weather = df_weather.groupby('weather_station_code')
+
+  def cutoff_weather(row):
+    try:
+      times = df_weather.time.ix[grouped_weather.groups[row[1]]]
+    except KeyError:
+      # Destination airport has no weather reports
+      return np.nan
+    try:
+      last_index = times[times < row[0]].idxmax()
+    except ValueError:
+      # No reports before the cutoff time
+      return np.nan
+    result = {}
+    for col in INTERESTING_WEATHER_COLUMNS:
+      result[ModifyWeatherColumnName(col)] = df_weather[col][last_index]
+    return pd.Series(result)
+
+  weather_info = df_joined[['cutoff_time', 'arrival_airport_icao_code']].apply(cutoff_weather, axis=1)
+  for name in weather_info.columns:
+    df_joined[name] = weather_info[name]
+
 def GenerateOneDay(parent_dir, date_str):
   input_dir = os.path.join(parent_dir, date_str.replace('-', '_'), 'good')
   output_dir = os.path.join(parent_dir, date_str.replace('-', '_'), 'model')
@@ -32,6 +60,7 @@ def GenerateOneDay(parent_dir, date_str):
 
   df_flights = pd.read_csv(os.path.join(input_dir, 'flighthistory.csv'), index_col='flight_history_id')
   df_features = pd.read_csv(os.path.join(input_dir, 'history_features.csv'), index_col='flight_history_id')
+  df_weather = pd.read_csv(os.path.join(input_dir, 'metar_weather.csv'), index_col='metar_reports_id')
 
   df_joined = df_flights.join(df_features)
   # Some data entries have NaN, few have <0 (?). Filter them out.
@@ -43,6 +72,7 @@ def GenerateOneDay(parent_dir, date_str):
 
   df_events = pd.read_csv(os.path.join(input_dir, 'history_event_features.csv'))
   AddLastEstimationTimes(df_joined, df_events, cutoff_series=df_joined.cutoff_time)
+  AddLastWeatherReportInDestination(df_joined, df_weather)
   flight_fields = ['scheduled_flight_time',
                    'scheduled_runway_arrival',
                    'scheduled_gate_arrival',
@@ -54,17 +84,12 @@ def GenerateOneDay(parent_dir, date_str):
                    'last_era_update',
                    'last_ega_update',
                    'last_era_update_time',
-                   'last_ega_update_time']
+                   'last_ega_update_time'] + map(ModifyWeatherColumnName, INTERESTING_WEATHER_COLUMNS)
   df_joined.to_csv(os.path.join(output_dir, 'training.csv'), cols=flight_fields)
-
-  metar_file = os.path.join(input_dir, 'metar_weather.csv')
-  df_weather = pd.read_csv(metar_file, index_col='metar_reports_id')
-  output_file = os.path.join(output_dir, 'per_flight_weather.csv')
-  GenerateWeatherPerFlight(df_joined, df_weather, output_file)
 
 
 def main():
-  GenerateOneDay(local_constants.PARENT_DATA_DIR, '2012-11-%02d' % 13)
+  GenerateOneDay(local_constants.PARENT_DATA_DIR, '2012-11-%02d' % 12)
   return
   for n in range(12, 26):
     GenerateOneDay(local_constants.PARENT_DATA_DIR, '2012-11-%02d' % n)
